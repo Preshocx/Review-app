19a7376b34db95ecc265884c2fff0052d5ac005d
Preshocx <109714203+Preshocx@users.noreply.github.com>
2023-05-20 00:28:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import random
 
-st.write('Hello world')
+def generate_random_number():
+    return random.randint(1, 100)
+
+def main():
+    st.title("Random Number Generator")
+    st.write("Click the button below to generate a random number between 1 and 100.")
+
+    if st.button("Generate"):
+        random_number = generate_random_number()
+        st.write(f"Random number: {random_number}")
+
+if __name__ == '__main__':
+    main()
